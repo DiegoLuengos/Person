97fc83059e46fb5da3033369382cf03e57f7444e
Diego <klosey88@hotmail.com>
2014-10-27 09:19:03 +0100

    atributo age añadido a la clase person

--- Person.java
+++ Person.java
@@ -7,27 +7,13 @@
  */
 public class Person
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private int age;
 
     /**
      * Constructor for objects of class Person
      */
     public Person()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+       
     }
 }
